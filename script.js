a777749e7df3ee3bb39c3c54e268e264819b3a15
rayyanurfitri1299-svg <rayyanurfitri1299@gmail.com>
2026-08-16 05:01:06 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 
+const AD_LINKS = {
+  smartlink: "https://bony-teaching.com/jLprYt"
+};
+
+const openAdButton = document.getElementById("openAd");
+
+openAdButton.addEventListener("click", function () {
+  window.open(
+    AD_LINKS.smartlink,
+    "_blank",
+    "noopener,noreferrer"
+  );
+});
