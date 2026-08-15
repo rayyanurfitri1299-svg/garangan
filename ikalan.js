46dc8bb2a78e8f93309a3bc86c504acfe3f2a4ce
rayyanurfitri1299-svg <rayyanurfitri1299@gmail.com>
2026-08-16 04:45:42 +0700

    Delete ikalan.js

--- ikalan.js
+++
@@ -1,9 +0,0 @@
-const intervalIklan = 15000;
-
-function jalankanIklan() {
-    console.log("Iklan dijalankan");
-    
-    // Masukkan kode iklan Anda di sini
-}
-
-setInterval(jalankanIklan, intervalIklan);
