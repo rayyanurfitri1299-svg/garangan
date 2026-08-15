6032a567aff4a799012c16795c1020d26319bb82
rayyanurfitri1299-svg <rayyanurfitri1299@gmail.com>
2026-08-15 22:31:50 +0700

    Create ikalan.js

---
+++ ikalan.js
@@ -0,0 +1,9 @@
+const intervalIklan = 15000;
+
+function jalankanIklan() {
+    console.log("Iklan dijalankan");
+    
+    // Masukkan kode iklan Anda di sini
+}
+
+setInterval(jalankanIklan, intervalIklan);
